0819a37fa5a2b2ce1c005ae725844254af1674b0
Stephen2Page <stephen2page@gmail.com>
2016-09-13 23:04:14 -0400

    final version

---
+++ DayOneProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayOneProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello " + name + "!");
+
+            Console.WriteLine("Enter two numbers to be added together.");
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+
+            int finalNumber = (numberOne + numberTwo);
+
+            Console.WriteLine("Your total is "+finalNumber);
+                     
+        }
+    }
+}
